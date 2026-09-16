ebe38aeb82a155c6f08d17de52e2565303798e74
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-16 10:46:54 +0530

    print l to r

---
+++ week2/printntor.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+
+    for (int i = a; i <= b; i++){
+        cout << i<< " ";
+    }
+    cout << endl;
+
+    return 0;
+}
