e7bd1b802d0e8bd3dc2353d596c068b609fde50e
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-16 09:20:24 +0530

    cpp week1 problem2

---
+++ week1/first5letters.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+
+int main(){
+    char a = 'A';
+    for (int i = 0; i < 5; i++){
+        cout << a << endl; 
+        a ++;
+    }
+
+    return 0;
+}
