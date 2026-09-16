064bb7ae62091912de0d25ab043d34cde410cbf7
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-16 10:48:59 +0530

    print all uppercase

---
+++ week2/printalluppercasealpha.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    char a = 'A';
+    while (a <= 'Z'){
+        cout << a<< " ";
+        a++;
+    }
+    cout << endl;
+
+    return 0;
+}
