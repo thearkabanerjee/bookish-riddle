12ffbedbe6aef298bf0ddfc4e6948e9f8b94f53f
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-17 10:30:40 +0530

    contest 1

---
+++ contest1/vowel.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    char a;
+    cin >> a;
+
+    if (a == 'a' || a =='e' || a== 'i' || a== 'o' || a == 'u'){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO"<< endl;
+    }
+
+    return 0;
+}
