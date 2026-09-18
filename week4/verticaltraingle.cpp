717a16be17da33b769e6907ffedf8bf527967193
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-18 11:48:46 +0530

    vertical triangle

--- week4/verticaltraingle.cpp
+++ week4/verticaltraingle.cpp
@@ -8,14 +8,14 @@ int main(){
 
     for (int i = 1; i <= a; i++){
         for (int j = 1; j <= i; j++ ){
-            cout << "*";
+            cout << "*" << " ";
         }
         cout << endl;
     }
 
     for (int i = a-1; i >= 1; i--){
         for (int j = 1; j <= i; j++){
-            cout << "*";
+            cout << "*" << " ";
         }
         cout << endl;
     
