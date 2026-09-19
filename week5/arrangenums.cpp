fea18b33605361e8c54e584b5a4359b6c1e7ca40
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-19 09:53:04 +0530

    reverse

---
+++ week5/arrangenums.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int m; 
+    cin >> m;
+
+    for (int z = 0; z < m; z++){
+        int x;
+        cin << x;
+    }
+
+
+    return 0;
+}
--- week5/reverse.cpp
+++ week5/reverse.cpp
@@ -1,4 +1,6 @@
 # include <iostream>
+# include <algorithm>
+
 using namespace std;
 
 int main(){
