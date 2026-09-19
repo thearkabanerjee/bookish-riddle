a40fb42eae975ea41b4486b75ab35d836213a7f9
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-19 09:34:08 +0530

    check if sorted

---
+++ week5/checkarraysorted.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin >> a;
+    int arr[a];
+
+    for (int i = 0; i < a; i++){
+        int m;
+        cin >> m;
+        arr[i] = m;
+    }
+
+
+    string verdict = "YES";
+    for(int i = 1; i < a; i++){
+        if (arr[i] < arr[i-1]){
+            verdict = "NO";
+            break;
+        }
+    }
+
+    cout << verdict << endl;
+    return 0;
+}
