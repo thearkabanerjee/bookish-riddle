8a1c1b96f2d49d7fc09f43dfd3ea9c19b7ca7766
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-19 09:57:42 +0530

    arrange nums

--- week5/arrangenums.cpp
+++ week5/arrangenums.cpp
@@ -7,7 +7,19 @@ int main(){
 
     for (int z = 0; z < m; z++){
         int x;
-        cin << x;
+        cin >> x;
+
+        for (int i = 1; i <= x; i++){
+            if (i % 2 != 0){
+                cout << i << " ";
+            }
+        }
+        for (int j = x; j >= 2; j-- ){
+            if (j % 2 == 0){
+                cout << j << " ";
+            }
+        }
+        cout << endl;
     }
 
 
