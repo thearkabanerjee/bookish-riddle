08524ff495c893df92ed16e3331ce94f7719dbfb
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-19 09:48:56 +0530

    reverse

---
+++ week5/reverse.cpp
@@ -0,0 +1,22 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin >> a;
+    int arr[a];
+
+    for (int i = 0; i < a;i++){
+        int m;
+        cin >> m;
+        arr[i] = m;
+    }
+    
+    reverse(arr, arr+a);
+
+    for (int i = 0; i < a; i++){
+        cout << arr[i] << " ";
+    }
+    cout <<endl;
+    return 0;
+}
