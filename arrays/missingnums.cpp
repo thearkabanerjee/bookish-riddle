588cbef6fceb95a264f7e69bd674828a21ce1fae
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-20 06:17:13 +0530

    arrays

--- arrays/missingnums.cpp
+++ arrays/missingnums.cpp
@@ -10,7 +10,26 @@ int main(){
         cin >> a;
         int arr[a];
 
-        for (int j = 0; i )
+        for (int j = 0; j < a; j++){
+            int m;
+            cin >> m;
+            arr[j] = m;
+        }
+
+        // int result = arr[0];
+        for (int i = 0; i < a; i++){
+            int count = 0;
+            for (int z = 0; z < a; z ++){
+                if (arr[i] == arr[z]){
+                    count ++;
+                }
+            }
+
+            if (count == 1){
+                cout << arr[i] << endl;
+                break;
+            }
+        }
 
     }
 
