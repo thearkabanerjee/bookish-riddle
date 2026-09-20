9e4c0881128d569c1a3bf0e583090932a42938bd
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-20 23:59:40 +0530

    contest 2 problem 8

---
+++ contest2/almostprimes.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int prime(int a) {
+    int counter = 0;
+
+    for (int i = 1; i <= a; i++) {
+        if (a % i == 0) {
+            counter++;
+        }
+    }
+
+    if (counter <= 4) {
+        return a;
+    }
+
+    return -1;
+}
+
+int main() {
+    int a;
+    cin >> a;
+
+    for (int i = 1; i <= a; i++) {
+        int result = prime(i);
+
+        if (result != -1) {
+            cout << result << " ";
+        }
+    }
+
+    cout << endl;
+
+    return 0;
+}
