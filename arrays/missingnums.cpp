183e0dcef4c5e51d93d956902ca55514e7a842b7
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-20 05:56:51 +0530

    folder renaming

---
+++ arrays/missingnums.cpp
@@ -0,0 +1,19 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int z;
+    cin >>z;
+    
+    for (int i = 0; i < z; i++){
+        int a;
+        cin >> a;
+        int arr[a];
+
+        for (int j = 0; i )
+
+    }
+
+
+    return 0;
+}
