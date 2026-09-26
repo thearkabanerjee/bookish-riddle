86180072995b43e98199ca3544dc854ef4a5674f
Arka Banerjee <arkabanerjee381@gmail.com>
2026-09-26 07:56:59 +0530

    day start

---
+++ sub-practice/duplicate-removing.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin >> a;
+    int arr[a];
+
+    for (int i = 0; i < a; i++){
+        cin >> arr[i];
+    }
+    
+    return 0;
+}
